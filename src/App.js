97d98578bb2c396c3e04ddf8906ad9b99ab403ce
Thạch Thiên <maihothienthach12a1thpt@gmail.com>
2024-08-25 17:26:58 +0700

    lab 01

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import Exercise1 from './components/component01/Exercise1';
 import Exercise2 from './components/component02/Exercise2';
 import Exercise3 from './components/component03/Exercise03';
+import Exercise4 from './components/component04/Exercise04';
+import Exercise5 from './components/component05/Exercise05';
+import Exercise6 from './components/component06/Exercise06';
+import Exercise7 from './components/component07/Exercise07';
+import Exercise8 from './components/component08/Exercise08';
+import Exercise9 from './components/component09/Exercise09';
+import Exercise10 from './components/component10/Exercise10';
+import Exercise11 from './components/component11/Exercise11';
 function App() {
   return (
     <div className="App">
+      Mai Ho Thien Thach
+      <br></br>
+      21005071
       <Exercise1/>
       <Exercise2/>
       <Exercise3/>
+      <Exercise4/>
+      <Exercise5/>
+      <Exercise6/>
+      <Exercise7/>
+      <Exercise8/>
+      <Exercise9/>
+      <Exercise10/>
+      <Exercise11/>
 
     </div>
   );
